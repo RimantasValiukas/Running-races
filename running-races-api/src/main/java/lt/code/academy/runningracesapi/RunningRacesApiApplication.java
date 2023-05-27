f7b9b82084349c811245bcf45857ce7e9b6c4442
RimantasValiukas <rimantas.valiukas@gmail.com>
2023-05-27 10:13:19 +0300

    Running races project started

---
+++ running-races-api/src/main/java/lt/code/academy/runningracesapi/RunningRacesApiApplication.java
@@ -0,0 +1,13 @@
+package lt.code.academy.runningracesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningRacesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunningRacesApiApplication.class, args);
+    }
+
+}
---
+++ running-races-api/src/test/java/lt/code/academy/runningracesapi/RunningRacesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.code.academy.runningracesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningRacesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
