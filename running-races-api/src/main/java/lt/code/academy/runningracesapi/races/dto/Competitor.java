58dd4b33e3aa89e5c55f72be85992a5e5b60a5d3
RimantasValiukas <rimantas.valiukas@gmail.com>
2023-06-03 14:37:13 +0300

    Competitor object and entity created

---
+++ running-races-api/src/main/java/lt/code/academy/runningracesapi/races/dto/Competitor.java
@@ -0,0 +1,39 @@
+package lt.code.academy.runningracesapi.races.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lt.code.academy.runningracesapi.races.entity.CompetitorEntity;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Competitor {
+    private UUID id;
+    private String name;
+    private String surname;
+    private Timestamp dateOfBirth;
+    private String city;
+    private String club;
+    private Double distance;
+    private Timestamp result;
+
+    public static Competitor convert(CompetitorEntity entity) {
+        return new Competitor(
+                entity.getCompetitorId(),
+                entity.getName(),
+                entity.getSurname(),
+                entity.getDateOfBirth(),
+                entity.getCity(),
+                entity.getClub(),
+                entity.getDistance(),
+                entity.getResult()
+        );
+    }
+
+}
---
+++ running-races-api/src/main/java/lt/code/academy/runningracesapi/races/entity/CompetitorEntity.java
@@ -0,0 +1,55 @@
+package lt.code.academy.runningracesapi.races.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lt.code.academy.runningracesapi.races.dto.Competitor;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "competitors")
+public class CompetitorEntity {
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "uuid", updatable = false)
+    private UUID competitorId;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String surname;
+    @Column(nullable = false)
+    private Timestamp dateOfBirth;
+    @Column(nullable = false)
+    private String city;
+    @Column
+    private String club;
+    @Column(nullable = false)
+    private Double distance;
+    @Column
+    private Timestamp result;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "raceId")
+    private RaceEntity raceEntity;
+
+    public static CompetitorEntity convert(Competitor competitor) {
+        return new CompetitorEntity(
+                competitor.getId(),
+                competitor.getName(),
+                competitor.getSurname(),
+                competitor.getDateOfBirth(),
+                competitor.getCity(),
+                competitor.getClub(),
+                competitor.getDistance(),
+                competitor.getResult(),
+                null
+        );
+    }
+
+}
