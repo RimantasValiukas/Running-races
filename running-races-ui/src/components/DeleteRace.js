743e9893ff37de68cab1a77c06ed4904092c997c
RimantasValiukas <rimantas.valiukas@gmail.com>
2023-06-05 18:18:26 +0300

    Comfirmation to DeleteRace component added

--- running-races-ui/src/components/DeleteRace.js
+++ running-races-ui/src/components/DeleteRace.js
@@ -1,30 +1,40 @@
 import {NavLink, useNavigate} from "react-router-dom";
-import {Alert, Button} from "@mui/material";
+import {Alert, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from "@mui/material";
 import {deleteRace} from "./api/raceApi";
 import {useState} from "react";
 
 const DeleteRace = ({raceId}) => {
 
-    const [message, setMessage] = useState({isVisible: false});
     const navigation = useNavigate();
+    const [open, setOpen] = useState(false);
 
+    const handleClose = () => setOpen(false);
+    const handleOpen = () => setOpen(true);
     const onRaceDelete = () => {
         deleteRace(raceId)
-            .then(() => {
-                setMessage({isVisible: true, message: 'Race deleted successfully', severity: 'success'})
-                setTimeout(() => navigation('/'), 1000);
-            })
-            .catch((error) => setMessage({isVisible: true, message: 'Race cannot be deleted', severity: 'error'}))
+            .then(() => navigation('/'))
+            .catch((error) => console.log(error));
     }
 
     return (
         <>
-            <Button size="small"
-                    color='warning'
-                    onClick={() => onRaceDelete()}>
+            <Button size="small" color="warning" onClick={handleOpen}>
                 Delete
             </Button>
-            {message.isVisible && <Alert severity={message.severity}>{message.message}</Alert>}
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Confirm Delete</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Are you sure you want to delete this race?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={onRaceDelete} color="error">
+                        Delete
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </>
     );
 }
