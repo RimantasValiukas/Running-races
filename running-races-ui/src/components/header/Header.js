f80eff83cfb29a948c7fa59147bc871dc23b9e58
RimantasValiukas <rimantas.valiukas@gmail.com>
2023-06-13 19:04:50 +0300

    user profile added

--- running-races-ui/src/components/header/Header.js
+++ running-races-ui/src/components/header/Header.js
@@ -1,16 +1,48 @@
-import React from 'react';
-import { AppBar, Box, Button, IconButton, Toolbar, Typography, Menu, MenuItem, Link, useMediaQuery, useTheme } from '@mui/material';
-import { NavLink } from 'react-router-dom';
+import React, {useState} from 'react';
+import {
+    AppBar,
+    Box,
+    Button,
+    IconButton,
+    Toolbar,
+    Typography,
+    Menu,
+    MenuItem,
+    Link,
+    useMediaQuery,
+    useTheme,
+    Tooltip, Avatar, Divider, ListItemIcon
+} from '@mui/material';
+import {NavLink} from 'react-router-dom';
 import MenuIcon from '@mui/icons-material/Menu';
 import LanguageSwitcher from "../switcher/LanguageSwitcher";
 import {useTranslation} from "react-i18next";
 import MenuBarItem from "./MenuBarItem";
+import {useDispatch, useSelector} from "react-redux";
+import {Logout, Settings} from "@mui/icons-material";
+import {removeUser} from "../../store/slices/userSlice";
 
 const Header = () => {
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [anchorEl, setAnchorEl] = useState(null);
     const {t} = useTranslation('header');
+    const user = useSelector(state => state.user.user);
+    const open = Boolean(anchorEl);
+    const dispatch = useDispatch();
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const onLogout = () => {
+        dispatch(removeUser);
+    }
+
 
     const handleMenuOpen = (event) => {
         setAnchorEl(event.currentTarget);
@@ -23,10 +55,11 @@ const Header = () => {
     return (
         <AppBar
             position="fixed"
-            sx={{ backgroundColor: '#112D4E', borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+            sx={{backgroundColor: '#112D4E', borderBottom: (theme) => `1px solid ${theme.palette.divider}`}}
         >
-            <Toolbar sx={{ flexWrap: 'wrap' }}>
-                <Typography variant="h6" color="inherit" noWrap component={NavLink} to="/" sx={{ flexGrow: 1, textDecoration: 'unset', color: '#F9F7F7' }}>
+            <Toolbar sx={{flexWrap: 'wrap'}}>
+                <Typography variant="h6" color="inherit" noWrap component={NavLink} to="/"
+                            sx={{flexGrow: 1, textDecoration: 'unset', color: '#F9F7F7'}}>
                     RunRaceBook
                 </Typography>
                 {isMobile ? (
@@ -36,9 +69,9 @@ const Header = () => {
                             aria-label="menu"
                             onClick={handleMenuOpen}
                             edge="end"
-                            sx={{ display: { sm: 'block', md: 'none' } }}
+                            sx={{display: {sm: 'block', md: 'none'}}}
                         >
-                            <MenuIcon />
+                            <MenuIcon/>
                         </IconButton>
                         <Menu
                             anchorEl={anchorEl}
@@ -71,22 +104,22 @@ const Header = () => {
                                     },
                                 },
                             }}
-                            transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                            transformOrigin={{horizontal: 'right', vertical: 'top'}}
+                            anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
                         >
-                            <MenuItem component={NavLink} to="/" sx={{ color: '#112D4E' }}>
+                            <MenuItem component={NavLink} to="/" sx={{color: '#112D4E'}}>
                                 {t('home')}
                             </MenuItem>
-                            <MenuItem component={NavLink} to="/races/previous" sx={{ color: '#112D4E' }}>
+                            <MenuItem component={NavLink} to="/races/previous" sx={{color: '#112D4E'}}>
                                 {t('previous')}
                             </MenuItem>
-                            <MenuItem component={NavLink} to="/races/create" sx={{ color: '#112D4E' }}>
+                            {user && <MenuItem component={NavLink} to="/races/create" sx={{color: '#112D4E'}}>
                                 {t('create')}
-                            </MenuItem>
-                            <MenuItem component={NavLink} to="/registration" sx={{ color: '#112D4E' }}>
+                            </MenuItem>}
+                            <MenuItem component={NavLink} to="/registration" sx={{color: '#112D4E'}}>
                                 {t('registration')}
                             </MenuItem>
-                            <MenuItem component={NavLink} to="/login" sx={{ color: '#112D4E' }}>
+                            <MenuItem component={NavLink} to="/login" sx={{color: '#112D4E'}}>
                                 {t('login')}
                             </MenuItem>
 
@@ -94,15 +127,88 @@ const Header = () => {
                     </>
                 ) : (
                     <>
-                            <MenuBarItem path='/' name={t('home')} />
-                            <MenuBarItem path='/races/previous' name={t('previous')}/>
-                            <MenuBarItem path="/races/create" name={t('create')}/>
-                            <MenuBarItem path="/registration" name= {t('registration')}/>
+                        <MenuBarItem path='/' name={t('home')}/>
+                        <MenuBarItem path='/races/previous' name={t('previous')}/>
+                        {user && <MenuBarItem path="/races/create" name={t('create')}/>}
+                        <MenuBarItem path="/registration" name={t('registration')}/>
 
                         <LanguageSwitcher/>
-                        <Button to="/login" component={NavLink} variant="outlined" sx={{ my: 1, mx: 1.5, color: '#F9F7F7', borderColor: '#F9F7F7' }}>
-                            {t('login')}
-                        </Button>
+
+                        {
+                            user ?
+                                <>
+                                    <Tooltip title="Account settings">
+                                        <IconButton
+                                            onClick={handleClick}
+                                            size="small"
+                                            sx={{mx: 2}}
+                                            aria-controls={open ? 'account-menu' : undefined}
+                                            aria-haspopup="true"
+                                            aria-expanded={open ? 'true' : undefined}
+                                        >
+                                            <Avatar sx={{width: 32, height: 32}}></Avatar>
+                                        </IconButton>
+                                    </Tooltip>
+                                    <Menu
+                                        anchorEl={anchorEl}
+                                        id="account-menu"
+                                        open={open}
+                                        onClose={handleClose}
+                                        onClick={handleClose}
+                                        PaperProps={{
+                                            elevation: 0,
+                                            sx: {
+                                                overflow: 'visible',
+                                                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                                mt: 1.5,
+                                                '& .MuiAvatar-root': {
+                                                    width: 32,
+                                                    height: 32,
+                                                    ml: -0.5,
+                                                    mr: 1,
+                                                },
+                                                '&:before': {
+                                                    content: '""',
+                                                    display: 'block',
+                                                    position: 'absolute',
+                                                    top: 0,
+                                                    right: 14,
+                                                    width: 10,
+                                                    height: 10,
+                                                    bgcolor: 'background.paper',
+                                                    transform: 'translateY(-50%) rotate(45deg)',
+                                                    zIndex: 0,
+                                                },
+                                            },
+                                        }}
+                                        transformOrigin={{horizontal: 'right', vertical: 'top'}}
+                                        anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}>
+                                        <MenuItem>
+                                            <Avatar/> {user.fullName}
+                                        </MenuItem>
+                                        <Divider/>
+                                        <MenuItem onClick={handleClose}>
+                                            <ListItemIcon>
+                                                <Settings fontSize="small"/>
+                                            </ListItemIcon>
+                                            Settings
+                                        </MenuItem>
+                                        <MenuItem onClick={onLogout}>
+                                            <ListItemIcon>
+                                                <Logout fontSize="small"/>
+                                            </ListItemIcon>
+                                            {t('logout')}
+                                        </MenuItem>
+                                    </Menu>
+                                </>
+                                :
+                                <Button to="/login" component={NavLink} variant="outlined"
+                                        sx={{my: 1, mx: 1.5, color: '#F9F7F7', borderColor: '#F9F7F7'}}>
+                                    {t('login')}
+                                </Button>
+                        }
+
+
                     </>
                 )}
             </Toolbar>
